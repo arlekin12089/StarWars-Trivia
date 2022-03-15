71a70d27a78283fe18cee84ea35399a48650a14f
Arlekin1208 <arlekin12089@gmail.com>
2022-03-15 20:20:44 +0100

    fix names

--- scripts/data.js
+++ scripts/data.js
@@ -5,9 +5,9 @@ export async function getCharacter(character_name) {
     "C-3PO": "images/C-3PO.jpg",
     "R2-D2": "images/R2-D2.jpg",
     "Darth Vader": "images/Darth_Vader.jpg",
-    Chewbacca: "images/Chewbacca.jpg",
+    "Chewbacca": "images/Chewbacca.jpg",
     "Han Solo": "images/Han_Solo.jpg",
-    Yoda: "images/Yoda.jpg",
+    "Yoda": "images/Yoda.jpg",
     "Leia Organa": "images/Leia_Organa.jpg",
   };
   let characterResponse = await fetch(`https://swapi.dev/api/people/?search=${character_name}`);
